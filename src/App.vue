d44dc874af0c07a2c81533a92f6051a9062d3c83
Gustavo <gustavcampos73@gmail.com>
2021-09-10 19:13:42 -0300

    basic css

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,52 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div @click="start" class="container">
+    <i class="fas fa-bolt"></i>
+
+    <h1 class="title">React as fast as you can!</h1>
+  </div>
+  <!-- <Footer /> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Footer from './components/Footer.vue';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Footer },
+  methods: {},
+};
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Signika+Negative:wght@300&display=swap');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: white;
+}
+
+i {
+  color: white;
+  font-size: 100px;
+  margin-bottom: 30px;
+}
+
+.title {
+  padding: 20px;
+  background-color: #fc919b;
+  font-family: 'Signika Negative', sans-serif;
+  font-size: 50px;
+}
+
+.container {
+  height: 100vh;
+  cursor: pointer;
+  background-color: #0089bd;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <footer>
+      <p>Copyright &copy; 2021</p>
+    </footer>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+footer {
+  text-align: center;
+  background-color: #dddddd;
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 25px;
+
+  color: #0089bd;
+}
+</style>
